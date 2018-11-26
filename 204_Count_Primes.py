287eaf2356b0e5b39bf3942ed7d4a6d2b9e4c909
Yu-Hsun Chen <eason_chen@quantatw.com>
2018-11-26 13:46:28 +0800

    204

---
+++ 204_Count_Primes.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0 or n == 1:
+            return 0
+        lst = [True] * n
+        lst[0] = False
+        lst[1] = False
+        for i in range(2, n):
+            if lst[i] == False:
+                continue
+            else:
+                base = 2
+                while i * base < n:
+                    lst[i*base] = False
+                    base += 1
+               
+        return sum(lst)
